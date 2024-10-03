41bbb971a6213e3ed5830ce18e6603b3f641065c
ADOLFO77 <l23281861@toluca.tecnm.mx>
2024-10-03 11:41:40 -0600

    a

---
+++ rpg/Game.java
@@ -0,0 +1,45 @@
+package rpg;
+
+import rpg.entities.Player;
+import rpg.entities.enemy.Enemy;
+import rpg.entities.enemy.goblins.RookieGoblin;
+import rpg.entities.enemy.slimes.BasicSlime;
+
+import java.util.Random;
+
+
+public class Game {
+
+    private Player player;
+    private Enemy enemy;
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
+    }
+
+    public Game() {
+        this.player = new Player("Aventurero");
+        int enemyType = new Random().nextInt(3) + 1;
+        this.enemy = switch (enemyType) {
+            case 1 -> new RookieGoblin("Rookie Goblin");
+            case 2 -> new BasicSlime("Basic Slime");
+            default -> new Enemy("Otro enemigo");
+        };
+    }
+
+    public void startGame() {
+        while (player.isAlive() && enemy.isAlive()) {
+            player.attack(enemy);
+            if (enemy.isAlive()) {
+                enemy.attack(player);
+            }
+        }
+
+        if (player.isAlive()) {
+            System.out.println(player.getName() + " GANO el que mayor poder tuvo :DDDDDDD");
+        } else {
+            System.out.println(enemy.getName() + " Gano, Debes ser mas fuerte a la proxima");
+        }
+    }
+}
